5de725789dbb5c39290ec270437da7ef14161d18
jyonnliu <i@6-79.cn>
2023-04-27 17:12:06 +0800

    for gen-nr

--- generate_small_dataset.py
+++ generate_small_dataset.py
@@ -2,14 +2,14 @@ import json
 
 from UniTok import UniDep
 
-path = 'data/MIND-small-v2/{mode}-fake'
+path = 'data/MIND-small-v2/{mode}-fake-v3'
 
 modes = ['train', 'dev', 'test']
 
-allowed_users = json.load(open('data/MIND-small-v2/allowed_user.json'))
+allowed_users = set(json.load(open('data/MIND-small-v2/allowed_user.json')))
 
 for mode in modes:
     depot = UniDep(path.format(mode=mode))
-    depot.filter(lambda x: x in allowed_users, col=depot.id_col)
+    depot.filter(lambda x: x in allowed_users, col='uid')
     depot.export(path.format(mode=mode) + '-allowed')
 
